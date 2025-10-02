50321e25f4f8d6e3971eec871d306a710ada7c68
MyWurger <wdgwdg9@gmail.com>
2025-10-02 23:38:24 +0300

    CI шаг 8: ИСПРАВЛЕНИЯ: добавлена документация к проекту

--- tests/unit/test_advanced_operations.cpp
+++ tests/unit/test_advanced_operations.cpp
@@ -31,168 +31,168 @@ int main() {
 
     // Тесты для функции divi
     RUN_TEST("divi_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(divi(10.0, 2.0), 5.0);
-        ctx.expect_optional_equal(divi(15.0, 3.0), 5.0);
-        ctx.expect_optional_equal(divi(7.0, 2.0), 3.5);
+        ctx.expect_optional_equal(calculator::divi(10.0, 2.0), 5.0);
+        ctx.expect_optional_equal(calculator::divi(15.0, 3.0), 5.0);
+        ctx.expect_optional_equal(calculator::divi(7.0, 2.0), 3.5);
     });
 
     RUN_TEST("divi_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(divi(-10.0, 2.0), -5.0);
-        ctx.expect_optional_equal(divi(10.0, -2.0), -5.0);
-        ctx.expect_optional_equal(divi(-10.0, -2.0), 5.0);
+        ctx.expect_optional_equal(calculator::divi(-10.0, 2.0), -5.0);
+        ctx.expect_optional_equal(calculator::divi(10.0, -2.0), -5.0);
+        ctx.expect_optional_equal(calculator::divi(-10.0, -2.0), 5.0);
     });
 
     RUN_TEST("divi_by_zero", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(divi(5.0, 0.0));
-        ctx.expect_nullopt(divi(-5.0, 0.0));
-        ctx.expect_nullopt(divi(0.0, 0.0));
+        ctx.expect_nullopt(calculator::divi(5.0, 0.0));
+        ctx.expect_nullopt(calculator::divi(-5.0, 0.0));
+        ctx.expect_nullopt(calculator::divi(0.0, 0.0));
     });
 
     RUN_TEST("divi_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(divi(1.0, 3.0), 1.0/3.0);
-        ctx.expect_optional_equal(divi(0.5, 0.25), 2.0);
-        ctx.expect_optional_equal(divi(0.1, 0.01), 10.0);
+        ctx.expect_optional_equal(calculator::divi(1.0, 3.0), 1.0/3.0);
+        ctx.expect_optional_equal(calculator::divi(0.5, 0.25), 2.0);
+        ctx.expect_optional_equal(calculator::divi(0.1, 0.01), 10.0);
     });
 
     // Тесты для функции mod
     RUN_TEST("mod_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(mod(7.5, 2.0), 1.5);
-        ctx.expect_optional_equal(mod(10.0, 3.0), 1.0);
-        ctx.expect_optional_equal(mod(15.0, 4.0), 3.0);
+        ctx.expect_optional_equal(calculator::mod(7.5, 2.0), 1.5);
+        ctx.expect_optional_equal(calculator::mod(10.0, 3.0), 1.0);
+        ctx.expect_optional_equal(calculator::mod(15.0, 4.0), 3.0);
     });
 
     RUN_TEST("mod_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(mod(-7.5, 2.0), -1.5);
-        ctx.expect_optional_equal(mod(7.5, -2.0), 1.5);
-        ctx.expect_optional_equal(mod(-7.5, -2.0), -1.5);
+        ctx.expect_optional_equal(calculator::mod(-7.5, 2.0), -1.5);
+        ctx.expect_optional_equal(calculator::mod(7.5, -2.0), 1.5);
+        ctx.expect_optional_equal(calculator::mod(-7.5, -2.0), -1.5);
     });
 
     RUN_TEST("mod_by_zero", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(mod(5.0, 0.0));
-        ctx.expect_nullopt(mod(-5.0, 0.0));
-        ctx.expect_nullopt(mod(0.0, 0.0));
+        ctx.expect_nullopt(calculator::mod(5.0, 0.0));
+        ctx.expect_nullopt(calculator::mod(-5.0, 0.0));
+        ctx.expect_nullopt(calculator::mod(0.0, 0.0));
     });
 
     RUN_TEST("mod_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(mod(5.7, 2.1), std::fmod(5.7, 2.1));
-        ctx.expect_optional_equal(mod(0.5, 0.3), std::fmod(0.5, 0.3));
+        ctx.expect_optional_equal(calculator::mod(5.7, 2.1), std::fmod(5.7, 2.1));
+        ctx.expect_optional_equal(calculator::mod(0.5, 0.3), std::fmod(0.5, 0.3));
     });
 
     // Тесты для функции powd
     RUN_TEST("powd_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::2.0, 5.0), 32.0);
-        ctx.expect_optional_equal(calculator::3.0, 2.0), 9.0);
-        ctx.expect_optional_equal(calculator::4.0, 0.5), 2.0);
+        ctx.expect_optional_equal(calculator::powd(2.0, 5.0), 32.0);
+        ctx.expect_optional_equal(calculator::powd(3.0, 2.0), 9.0);
+        ctx.expect_optional_equal(calculator::powd(4.0, 0.5), 2.0);
     });
 
     RUN_TEST("powd_fractional", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::9.0, -0.5), 1.0 / 3.0);
-        ctx.expect_optional_equal(calculator::8.0, 1.0/3.0), 2.0);
-        ctx.expect_optional_equal(calculator::16.0, 0.25), 2.0);
+        ctx.expect_optional_equal(calculator::powd(9.0, -0.5), 1.0 / 3.0);
+        ctx.expect_optional_equal(calculator::powd(8.0, 1.0/3.0), 2.0);
+        ctx.expect_optional_equal(calculator::powd(16.0, 0.25), 2.0);
     });
 
     RUN_TEST("powd_invalid", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(calculator::-2.0, 0.5));  // Корень из отрицательного числа
-        ctx.expect_nullopt(calculator::0.0, -1.0));  // Деление на ноль
-        ctx.expect_nullopt(calculator::-1.0, 0.5));  // Корень из отрицательного числа
+        ctx.expect_nullopt(calculator::powd(-2.0, 0.5));  // Корень из отрицательного числа
+        ctx.expect_nullopt(calculator::powd(0.0, -1.0));  // Деление на ноль
+        ctx.expect_nullopt(calculator::powd(-1.0, 0.5));  // Корень из отрицательного числа
     });
 
     RUN_TEST("powd_zero", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::0.0, 5.0), 0.0);
-        ctx.expect_optional_equal(calculator::5.0, 0.0), 1.0);
-        ctx.expect_optional_equal(calculator::0.0, 0.0), 1.0);
+        ctx.expect_optional_equal(calculator::powd(0.0, 5.0), 0.0);
+        ctx.expect_optional_equal(calculator::powd(5.0, 0.0), 1.0);
+        ctx.expect_optional_equal(calculator::powd(0.0, 0.0), 1.0);
     });
 
     // Тесты для функции logb
     RUN_TEST("logb_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::8.0, 2.0), 3.0);
-        ctx.expect_optional_equal(calculator::9.0, 3.0), 2.0);
-        ctx.expect_optional_equal(calculator::100.0, 10.0), 2.0);
+        ctx.expect_optional_equal(calculator::logb(8.0, 2.0), 3.0);
+        ctx.expect_optional_equal(calculator::logb(9.0, 3.0), 2.0);
+        ctx.expect_optional_equal(calculator::logb(100.0, 10.0), 2.0);
     });
 
     RUN_TEST("logb_special", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::1.0, 10.0), 0.0);
-        ctx.expect_optional_equal(calculator::2.0, 2.0), 1.0);
-        ctx.expect_optional_equal(calculator::0.5, 2.0), -1.0);
+        ctx.expect_optional_equal(calculator::logb(1.0, 10.0), 0.0);
+        ctx.expect_optional_equal(calculator::logb(2.0, 2.0), 1.0);
+        ctx.expect_optional_equal(calculator::logb(0.5, 2.0), -1.0);
     });
 
     RUN_TEST("logb_invalid", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(calculator::-1.0, 10.0));  // Отрицательное значение
-        ctx.expect_nullopt(calculator::8.0, 1.0));    // Основание равно 1
-        ctx.expect_nullopt(calculator::8.0, 0.0));    // Основание равно 0
-        ctx.expect_nullopt(calculator::8.0, -2.0));   // Отрицательное основание
-        ctx.expect_nullopt(calculator::0.0, 10.0));   // Значение равно 0
+        ctx.expect_nullopt(calculator::logb(-1.0, 10.0));  // Отрицательное значение
+        ctx.expect_nullopt(calculator::logb(8.0, 1.0));    // Основание равно 1
+        ctx.expect_nullopt(calculator::logb(8.0, 0.0));    // Основание равно 0
+        ctx.expect_nullopt(calculator::logb(8.0, -2.0));   // Отрицательное основание
+        ctx.expect_nullopt(calculator::logb(0.0, 10.0));   // Значение равно 0
     });
 
     RUN_TEST("logb_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::2.0, 4.0), 0.5);
-        ctx.expect_optional_equal(calculator::0.25, 2.0), -2.0);
+        ctx.expect_optional_equal(calculator::logb(2.0, 4.0), 0.5);
+        ctx.expect_optional_equal(calculator::logb(0.25, 2.0), -2.0);
     });
 
     // Тесты для функции square_root
     RUN_TEST("square_root_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::4.0), 2.0);
-        ctx.expect_optional_equal(calculator::9.0), 3.0);
-        ctx.expect_optional_equal(calculator::16.0), 4.0);
-        ctx.expect_optional_equal(calculator::0.0), 0.0);
+        ctx.expect_optional_equal(calculator::square_root(4.0), 2.0);
+        ctx.expect_optional_equal(calculator::square_root(9.0), 3.0);
+        ctx.expect_optional_equal(calculator::square_root(16.0), 4.0);
+        ctx.expect_optional_equal(calculator::square_root(0.0), 0.0);
     });
 
     RUN_TEST("square_root_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::2.25), 1.5);
-        ctx.expect_optional_equal(calculator::0.25), 0.5);
-        ctx.expect_optional_equal(calculator::6.25), 2.5);
+        ctx.expect_optional_equal(calculator::square_root(2.25), 1.5);
+        ctx.expect_optional_equal(calculator::square_root(0.25), 0.5);
+        ctx.expect_optional_equal(calculator::square_root(6.25), 2.5);
     });
 
     RUN_TEST("square_root_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(calculator::-1.0));
-        ctx.expect_nullopt(calculator::-4.0));
-        ctx.expect_nullopt(calculator::-0.1));
+        ctx.expect_nullopt(calculator::square_root(-1.0));
+        ctx.expect_nullopt(calculator::square_root(-4.0));
+        ctx.expect_nullopt(calculator::square_root(-0.1));
     });
 
     RUN_TEST("square_root_large_numbers", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::1000000.0), 1000.0);
-        ctx.expect_optional_equal(calculator::100.0), 10.0);
+        ctx.expect_optional_equal(calculator::square_root(1000000.0), 1000.0);
+        ctx.expect_optional_equal(calculator::square_root(100.0), 10.0);
     });
 
     // Тесты для функции factorial
     RUN_TEST("factorial_normal", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::0.0), 1.0);   // 0! = 1
-        ctx.expect_optional_equal(calculator::1.0), 1.0);   // 1! = 1
-        ctx.expect_optional_equal(calculator::2.0), 2.0);    // 2! = 2
-        ctx.expect_optional_equal(calculator::3.0), 6.0);    // 3! = 6
-        ctx.expect_optional_equal(calculator::4.0), 24.0);   // 4! = 24
-        ctx.expect_optional_equal(calculator::5.0), 120.0);  // 5! = 120
+        ctx.expect_optional_equal(calculator::factorial(0.0), 1.0);   // 0! = 1
+        ctx.expect_optional_equal(calculator::factorial(1.0), 1.0);   // 1! = 1
+        ctx.expect_optional_equal(calculator::factorial(2.0), 2.0);    // 2! = 2
+        ctx.expect_optional_equal(calculator::factorial(3.0), 6.0);    // 3! = 6
+        ctx.expect_optional_equal(calculator::factorial(4.0), 24.0);   // 4! = 24
+        ctx.expect_optional_equal(calculator::factorial(5.0), 120.0);  // 5! = 120
     });
 
     RUN_TEST("factorial_larger_numbers", [](test_utils::TestContext& ctx) {
-        ctx.expect_optional_equal(calculator::6.0), 720.0);   // 6! = 720
-        ctx.expect_optional_equal(calculator::7.0), 5040.0);  // 7! = 5040
-        ctx.expect_optional_equal(calculator::8.0), 40320.0); // 8! = 40320
-        ctx.expect_optional_equal(calculator::10.0), 3628800.0); // 10! = 3628800
+        ctx.expect_optional_equal(calculator::factorial(6.0), 720.0);   // 6! = 720
+        ctx.expect_optional_equal(calculator::factorial(7.0), 5040.0);  // 7! = 5040
+        ctx.expect_optional_equal(calculator::factorial(8.0), 40320.0); // 8! = 40320
+        ctx.expect_optional_equal(calculator::factorial(10.0), 3628800.0); // 10! = 3628800
     });
 
     RUN_TEST("factorial_invalid", [](test_utils::TestContext& ctx) {
-        ctx.expect_nullopt(calculator::-1.0));    // Отрицательное число
-        ctx.expect_nullopt(calculator::-5.0));    // Отрицательное число
-        ctx.expect_nullopt(calculator::1.5));     // Нецелое число
-        ctx.expect_nullopt(calculator::3.7));     // Нецелое число
-        ctx.expect_nullopt(calculator::0.1));      // Нецелое число
+        ctx.expect_nullopt(calculator::factorial(-1.0));    // Отрицательное число
+        ctx.expect_nullopt(calculator::factorial(-5.0));    // Отрицательное число
+        ctx.expect_nullopt(calculator::factorial(1.5));     // Нецелое число
+        ctx.expect_nullopt(calculator::factorial(3.7));     // Нецелое число
+        ctx.expect_nullopt(calculator::factorial(0.1));      // Нецелое число
     });
 
     RUN_TEST("factorial_large_numbers", [](test_utils::TestContext& ctx) {
         // Тестируем граничные случаи
-        ctx.expect_optional_equal(calculator::15.0), 1307674368000.0); // 15! = 1307674368000
-        ctx.expect_nullopt(calculator::171.0));    // Слишком большое число
-        ctx.expect_nullopt(calculator::200.0));    // Слишком большое число
+        ctx.expect_optional_equal(calculator::factorial(15.0), 1307674368000.0); // 15! = 1307674368000
+        ctx.expect_nullopt(calculator::factorial(171.0));    // Слишком большое число
+        ctx.expect_nullopt(calculator::factorial(200.0));    // Слишком большое число
     });
 
     RUN_TEST("factorial_precision", [](test_utils::TestContext& ctx) {
         // Тестируем точность для чисел с плавающей точкой
-        ctx.expect_optional_equal(calculator::5.0), 120.0);      // Точно 5.0
-        ctx.expect_optional_equal(calculator::5.0000001), 120.0); // Почти 5.0
-        ctx.expect_optional_equal(calculator::4.9999999), 120.0); // Почти 5.0
-        ctx.expect_nullopt(calculator::5.1));      // Слишком далеко от целого
-        ctx.expect_nullopt(calculator::5.5));      // Половина
+        ctx.expect_optional_equal(calculator::factorial(5.0), 120.0);      // Точно 5.0
+        ctx.expect_optional_equal(calculator::factorial(5.0000001), 120.0); // Почти 5.0
+        ctx.expect_optional_equal(calculator::factorial(4.9999999), 120.0); // Почти 5.0
+        ctx.expect_nullopt(calculator::factorial(5.1));      // Слишком далеко от целого
+        ctx.expect_nullopt(calculator::factorial(5.5));      // Половина
     });
 
     if (failures == 0) {
--- tests/unit/test_basic_operations.cpp
+++ tests/unit/test_basic_operations.cpp
@@ -27,65 +27,65 @@ int main() {
 
     // Тесты для функции add
     RUN_TEST("add_positive", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::1.5, 2.5), 4.0);
-        ctx.expect_equal(calculator::0.0, 5.0), 5.0);
-        ctx.expect_equal(calculator::100.0, 200.0), 300.0);
+        ctx.expect_equal(calculator::add(1.5, 2.5), 4.0);
+        ctx.expect_equal(calculator::add(0.0, 5.0), 5.0);
+        ctx.expect_equal(calculator::add(100.0, 200.0), 300.0);
     });
 
     RUN_TEST("add_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::-3.0, 3.0), 0.0);
-        ctx.expect_equal(calculator::-5.0, -2.0), -7.0);
-        ctx.expect_equal(calculator::-10.0, 15.0), 5.0);
+        ctx.expect_equal(calculator::add(-3.0, 3.0), 0.0);
+        ctx.expect_equal(calculator::add(-5.0, -2.0), -7.0);
+        ctx.expect_equal(calculator::add(-10.0, 15.0), 5.0);
     });
 
     RUN_TEST("add_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::0.1, 0.2), 0.3);
-        ctx.expect_equal(calculator::1.1, 2.2), 3.3);
-        ctx.expect_equal(calculator::0.0001, 0.0002), 0.0003);
+        ctx.expect_equal(calculator::add(0.1, 0.2), 0.3);
+        ctx.expect_equal(calculator::add(1.1, 2.2), 3.3);
+        ctx.expect_equal(calculator::add(0.0001, 0.0002), 0.0003);
     });
 
     // Тесты для функции sub
     RUN_TEST("sub_positive", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::5.0, 2.0), 3.0);
-        ctx.expect_equal(calculator::10.0, 3.0), 7.0);
-        ctx.expect_equal(calculator::100.0, 50.0), 50.0);
+        ctx.expect_equal(calculator::sub(5.0, 2.0), 3.0);
+        ctx.expect_equal(calculator::sub(10.0, 3.0), 7.0);
+        ctx.expect_equal(calculator::sub(100.0, 50.0), 50.0);
     });
 
     RUN_TEST("sub_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::-2.0, -3.0), 1.0);
-        ctx.expect_equal(calculator::-5.0, 2.0), -7.0);
-        ctx.expect_equal(calculator::3.0, -2.0), 5.0);
+        ctx.expect_equal(calculator::sub(-2.0, -3.0), 1.0);
+        ctx.expect_equal(calculator::sub(-5.0, 2.0), -7.0);
+        ctx.expect_equal(calculator::sub(3.0, -2.0), 5.0);
     });
 
     RUN_TEST("sub_zero", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::5.0, 0.0), 5.0);
-        ctx.expect_equal(calculator::0.0, 5.0), -5.0);
-        ctx.expect_equal(calculator::0.0, 0.0), 0.0);
+        ctx.expect_equal(calculator::sub(5.0, 0.0), 5.0);
+        ctx.expect_equal(calculator::sub(0.0, 5.0), -5.0);
+        ctx.expect_equal(calculator::sub(0.0, 0.0), 0.0);
     });
 
     // Тесты для функции mul
     RUN_TEST("mul_positive", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::3.0, 4.0), 12.0);
-        ctx.expect_equal(calculator::5.0, 6.0), 30.0);
-        ctx.expect_equal(calculator::10.0, 10.0), 100.0);
+        ctx.expect_equal(calculator::mul(3.0, 4.0), 12.0);
+        ctx.expect_equal(calculator::mul(5.0, 6.0), 30.0);
+        ctx.expect_equal(calculator::mul(10.0, 10.0), 100.0);
     });
 
     RUN_TEST("mul_negative", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::-2.0, 5.0), -10.0);
-        ctx.expect_equal(calculator::3.0, -4.0), -12.0);
-        ctx.expect_equal(calculator::-3.0, -4.0), 12.0);
+        ctx.expect_equal(calculator::mul(-2.0, 5.0), -10.0);
+        ctx.expect_equal(calculator::mul(3.0, -4.0), -12.0);
+        ctx.expect_equal(calculator::mul(-3.0, -4.0), 12.0);
     });
 
     RUN_TEST("mul_zero", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::0.0, 5.0), 0.0);
-        ctx.expect_equal(calculator::5.0, 0.0), 0.0);
-        ctx.expect_equal(calculator::0.0, 0.0), 0.0);
+        ctx.expect_equal(calculator::mul(0.0, 5.0), 0.0);
+        ctx.expect_equal(calculator::mul(5.0, 0.0), 0.0);
+        ctx.expect_equal(calculator::mul(0.0, 0.0), 0.0);
     });
 
     RUN_TEST("mul_decimal", [](test_utils::TestContext& ctx) {
-        ctx.expect_equal(calculator::0.5, 4.0), 2.0);
-        ctx.expect_equal(calculator::2.5, 2.0), 5.0);
-        ctx.expect_equal(calculator::0.1, 0.1), 0.01);
+        ctx.expect_equal(calculator::mul(0.5, 4.0), 2.0);
+        ctx.expect_equal(calculator::mul(2.5, 2.0), 5.0);
+        ctx.expect_equal(calculator::mul(0.1, 0.1), 0.01);
     });
 
     if (failures == 0) {
